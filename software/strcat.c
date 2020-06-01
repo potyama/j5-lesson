eb1cf9f9235e9645c3459f6df645668571f91938
potyama <kamikaze117117@gmail.com>
2020-06-01 15:30:30 +0900

    add software

--- software/strcat.c
+++ software/strcat.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-#include <string.h>
+
 
 char *Strcat(char *s, char *p) {
-  char *s_ptr = s;
-  while (*s_ptr != '\0') {
-    s_ptr++;
+  while (*s != '\0') {
+    s++;
   }
   while (*p != '\0') {
-    *s_ptr++ = *p++;
+    *s++ = *p++;
   }
-  *s_ptr = '\0';
+  *s = '\0';
   return s;
 }
 
 int main() {
-  char s[10] = "Hi";
-  printf("s = %s\n", s);
-  char *p = Strcat(s, "ramen!");
-  printf("s = %s\n", s);
+    char s[10] = "Hi";
+      printf("s = %s\n", s);
+        char *p = Strcat(s, "ramen!");
+          printf("s = %s\n", s);
 }
