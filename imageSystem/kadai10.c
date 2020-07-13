a5922a8330f7c10099dfedd51397a1de606f4441
potyama <kamikaze117117@gmail.com>
2020-07-13 15:58:23 +0900

    add median filter

--- imageSystem/kadai10.c
+++ imageSystem/kadai10.c
@@ -10,9 +10,9 @@
 #include "var.h"
 #include "bmpfile.h"
 
-int bsort(double c[], int cnt){
-    for (int i = 0; i < cnt; i++) {
-        for (int j = 0; i < cnt; j++) {
+int bsort(unsigned char c[], int cnt){
+    for(int j = 0;j < cnt-1; j++){
+        for (int i = 0; i < cnt-1; i++) {
             if (c[i+1] < c[i]) {
                 int tmp = c[i+1];
                 c[i+1] = c[i];
@@ -20,31 +20,27 @@ int bsort(double c[], int cnt){
             }
         }
     }
-    if(cnt% 2 == 0){
-        return (c[cnt/2] +c[((cnt/2)+1)/2] );
-    }
+    if(cnt% 2 == 0)
+        return (c[(cnt/2)-1] + c[cnt/2])/2;
 
-    return c[(cnt/2) + 1];
+    return c[4];
 }
 
-int mean_value_filter(imgdata idata, int x, int y, int h_start, int h_stop, int w_start,int w_stop){
-    double sum_data = 0;
+double median_filter(imgdata idata, int x, int y, int h_start, int h_stop, int w_start,int w_stop){
     int cnt = 0;
-    double data[12] = {};
-
+    unsigned char data[12] = {};
     for(int i = h_start; i <= h_stop; i++){
         for(int j = w_start; j <= w_stop; j++){
             data[cnt] = idata.source[RED][y+i][x+j];
             cnt++;
         }
     }
-    printf("%d\n",bsort(data, cnt));
+
     return bsort(data,cnt);
 }
 
 int main(int argc, char *argv[]) {
     imgdata idata;
-    double c;
 
   // 例題プログラム
   // 　BMPファイルをコピーするプログラム
@@ -67,37 +63,34 @@ int main(int argc, char *argv[]) {
                 double sum_data = 0;
                 if(x == 0){
                     if(y == 0){
-                        sum_data = mean_value_filter(idata, x, y, 0, 1, 0, 1);
+                        sum_data = median_filter(idata, x, y, 0, 1, 0, 1);
                     }else if(y == 255){
-                        sum_data = mean_value_filter(idata, x, y, -1, 0, 0, 1);
+                        sum_data = median_filter(idata, x, y, -1, 0, 0, 1);
                     }else{
-                        sum_data = mean_value_filter(idata, x, y, -1, 1, 0, 1);
+                        sum_data = median_filter(idata, x, y, -1, 1, 0, 1);
                     }
                 }else if(x == 255){
                     if(y == 0){
-                        sum_data = mean_value_filter(idata, x, y, 0, 1, -1, 0);
+                        sum_data = median_filter(idata, x, y, 0, 1, -1, 0);
                     }else if(y == 255){
-                        sum_data = mean_value_filter(idata, x, y, -1, 0, -1, 0);
+                        sum_data = median_filter(idata, x, y, -1, 0, -1, 0);
                     }else{
-                        sum_data = mean_value_filter(idata, x, y, -1, 1, -1, 0);
+                        sum_data = median_filter(idata, x, y, -1, 1, -1, 0);
                     }
                 }else if(y == 0){
-                    sum_data = mean_value_filter(idata, x, y, 0, 1, -1, 1);
+                    sum_data = median_filter(idata, x, y, 0, 1, -1, 1);
                 }else if(y == 255){
-                    sum_data = mean_value_filter(idata, x, y, -1, 0, -1, 1);
+                    sum_data = median_filter(idata, x, y, -1, 0, -1, 1);
                 }else{
                     //関数に渡すよりも、ここで直接処理したほうが早い(自社調べ)
-                    double sum_data = 0;
                     int cnt = 0;
-                    double data[12] = {};
-                    printf("aaa");
+                    unsigned char data[12] = {};
                     for(int i = (-1); i <= 1; i++){
                         for(int j = (-1); j <= 1; j++){
                             data[cnt] = idata.source[RED][y+i][x+j];
                             cnt++;
                         }
                     }
-                    printf("%d\n", bsort(data, cnt));
                     sum_data = bsort(data, cnt);
                 }
 
