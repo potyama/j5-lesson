7df025370c7652c5871807c91b95469246c178c0
potyama <kamikaze117117@gmail.com>
2020-07-14 01:09:22 +0900

    add median filter

--- imageSystem/kadai-10/kadai10.c
+++ imageSystem/kadai-10/kadai10.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
 #ifndef DEF_H
 
 #include "def.h"
@@ -10,37 +11,13 @@
 #include "var.h"
 #include "bmpfile.h"
 
-int bsort(unsigned char c[], int cnt){
-    for(int j = 0;j < cnt-1; j++){
-        for (int i = 0; i < cnt-1; i++) {
-            if (c[i+1] < c[i]) {
-                int tmp = c[i+1];
-                c[i+1] = c[i];
-                c[i] = tmp;
-            }
-        }
-    }
-    if(cnt% 2 == 0)
-        return (c[(cnt/2)-1] + c[cnt/2])/2;
-
-    return c[4];
-}
-
-double median_filter(imgdata idata, int x, int y, int h_start, int h_stop, int w_start,int w_stop){
-    int cnt = 0;
-    unsigned char data[12] = {};
-    for(int i = h_start; i <= h_stop; i++){
-        for(int j = w_start; j <= w_stop; j++){
-            data[cnt] = idata.source[RED][y+i][x+j];
-            cnt++;
-        }
-    }
-
-    return bsort(data,cnt);
+int compare_double(const void* a, const void* b){
+    return *(double*)a - *(double*)b;
 }
 
 int main(int argc, char *argv[]) {
     imgdata idata;
+    double N = 3;
 
   // 例題プログラム
   // 　BMPファイルをコピーするプログラム
@@ -58,50 +35,44 @@ int main(int argc, char *argv[]) {
             printf("指定コピー元ファイル%sが見つかりません\n",argv[1]);
         else {
         /* 課題9 : 入力画像を平均値フィルタ処理するプログラム */
-        for (int y = 0; y < idata.height; y++){
-            for (int x = 0; x < idata.width; x++){
-                double sum_data = 0;
-                if(x == 0){
-                    if(y == 0){
-                        sum_data = median_filter(idata, x, y, 0, 1, 0, 1);
-                    }else if(y == 255){
-                        sum_data = median_filter(idata, x, y, -1, 0, 0, 1);
-                    }else{
-                        sum_data = median_filter(idata, x, y, -1, 1, 0, 1);
-                    }
-                }else if(x == 255){
-                    if(y == 0){
-                        sum_data = median_filter(idata, x, y, 0, 1, -1, 0);
-                    }else if(y == 255){
-                        sum_data = median_filter(idata, x, y, -1, 0, -1, 0);
-                    }else{
-                        sum_data = median_filter(idata, x, y, -1, 1, -1, 0);
-                    }
-                }else if(y == 0){
-                    sum_data = median_filter(idata, x, y, 0, 1, -1, 1);
-                }else if(y == 255){
-                    sum_data = median_filter(idata, x, y, -1, 0, -1, 1);
-                }else{
-                    //関数に渡すよりも、ここで直接処理したほうが早い(自社調べ)
+            for(int y = 0; y < idata.height; y++){
+                for(int x = 0; x < idata.width; x++){
+                    int check = 0;
+                    double data[9] = {};
                     int cnt = 0;
-                    unsigned char data[12] = {};
-                    for(int i = (-1); i <= 1; i++){
-                        for(int j = (-1); j <= 1; j++){
-                            data[cnt] = idata.source[RED][y+i][x+j];
+
+                    for(int j = 0;j < N; j++){
+                        for(int i = 0; i < N; i++){
+                            int x_coordinate = i - (N-1)/2;
+                            int y_coordinate = j - (N-1)/2;
+                            if(y + y_coordinate < 0
+                            || y + y_coordinate >= idata.height
+                            || x + x_coordinate < 0
+                            || x + x_coordinate >= idata.width){
+                                check++;
+                                continue;
+                            }
+
+                            data[cnt] = idata.source[RED][y + y_coordinate][x + x_coordinate];
                             cnt++;
                         }
                     }
-                    sum_data = bsort(data, cnt);
-                }
+                    qsort(data, cnt, sizeof(double), compare_double);
 
-                idata.results[RED][y][x] = sum_data;
-                idata.results[GREEN][y][x] = sum_data;
-                idata.results[BLUE][y][x] = sum_data;
+                    double result;
+                    if(cnt%2){
+                        result = data[(cnt)/2];
+                    }else{
+                        result = (data[cnt/2]+data[(cnt-1)/2])/2;
+                    }
+
+                    idata.results[RED][y][x] = result;
+                    idata.results[GREEN][y][x] = result;
+                    idata.results[BLUE][y][x] = result;
+                }
             }
         }
-
         if (writeBMPfile(argv[2], &idata) > 0)
             printf("コピー先ファイル%sに保存できませんでした\n",argv[2]);
         }
-    }
 }
