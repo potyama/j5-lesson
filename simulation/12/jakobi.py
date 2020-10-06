5a6ae6b55823b8f2145e6a6214820e2c2accfa6b
potyama <kamikaze117117@gmail.com>
2020-10-07 00:57:37 +0900

    ヤコビ法

--- simulation/12/jakobi.py
+++ simulation/12/jakobi.py
@@ -1,25 +1,26 @@
 import numpy as np
 
-def jakobi(A, b):
+
+def jacobi_method(A, b):
+    # 線形連立方程式をヤコビ法で解く
+    x_old = np.empty_like(b)
+    err = 1e12
+
     D = np.diag(A)
     R = A - np.diagflat(D)
-    err = 1e-9
 
-    x_old = np.empty_like(b)
-
-    while 1e12 > err:
-        x = (b - np.dot(R, x_old)) / D
-        err = np.linalg.norm(x - x_old)/ np.linalg.norm(x)
+    while err > 1e-7:
+        x = (b-np.dot(R, x_old))/D
+        err = np.linalg.norm(x-x_old)/np.linalg.norm(x)
         x_old = x
 
     return x
 
-
-A = np.array([[7, 1, 2],
+A = np.array([[7, 1, 10],
               [1, 8, 3],
               [2, 3, 9]])
 
 b = np.array([10, 8, 6])
 
-x = jakobi(A, b)
+x = jacobi_method(A, b)
 print(x)
